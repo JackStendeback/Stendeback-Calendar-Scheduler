41a52c7c5c836560501d563a298537f031ad56a1
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-07-21 18:46:41 -0500

    minor changes in html, and added some acceptance criteria to follow

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const currentDate = dayjs().format("MMMM DD YYYY");
 document.getElementById("currentDay").textContent = currentDate;
 
 // When I scroll down, I am presented with time blocks for standard business hours of 9am to 5pm.
+// 'Added in HTML'
+
+//When I view the time blocks for that day, THEN each time block is color-coded to indicate whether it is in the past, present, or future.
+
+// When I click into a time block, THEN I can enter an event
+// This is already working with bootstrap in HTML. Text can be entered, it just doesn't save to local storage just yet, or stay on page refresh.
+
+// When I click the save button for that time block, THEN the text for that event is saved in local storage.
+
+
+// When I refresh the page, THEN the saved events persist.
