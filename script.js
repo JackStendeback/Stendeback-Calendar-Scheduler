6123b34dffdb81a1b47a86244563b072a250f28e
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-09-01 21:51:38 -0500

    adding functionality to actively update time blocks color based on time of day

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-// How would we make this visible on the screen on top of the calendar?
-// currentDay is the ID I want to attach in order to see the date in the proper place.
-
-// Implemented dayjs into a new variable 'currentDate' and connected that to 'currentDay' in my html
-const currentDate = dayjs().format("ddd, MMM D, YYYY h:mm A");
-document.getElementById("currentDay").textContent = currentDate;
+const currentDay = document.getElementById('current-day');
 
 
+function checkHour() {
+    const currentHour = dayjs().hour();
+    const textAreaBackgrounds = document.querySelectorAll('.time-block');
 
-// When I scroll down, I am presented with time blocks for standard business hours of 9am to 5pm.
-// 'Added in HTML'
-// created variable for time of day, and a variable for changing the content color.
-const currentHour = dayjs().hour();
-const contentColor = document.getElementById("hour-1");
+    textAreaBackgrounds.forEach((textAreaBackground) => {
+        
+        const timeBlockHour = parseInt(textAreaBackground.id.split('-')[1]);
 
-// Colors for past, present, and future times.
-const pastColor = 'grey';
-const presentColor = 'red';
-const futureColor = 'green';
+        const textArea = textAreaBackground.querySelector('.text-area-bg');
 
-// Updating color based on time during work day.
-
-
-//When I view the time blocks for that day, THEN each time block is color-coded to indicate whether it is in the past, present, or future.
-
-// When I click into a time block, THEN I can enter an event
-// This is already working with bootstrap in HTML. Text can be entered, it just doesn't save to local storage just yet, or stay on page refresh.
-
-// When I click the save button for that time block, THEN the text for that event is saved in local storage.
-// How do I connect the button & text box?
+        if (currentHour === timeBlockHour) {
+            textArea.classList.add('current');
+            textArea.classList.remove('past', 'future');
+        } else if (currentHour > timeBlockHour) {
+            textArea.classList.add('past');
+            textArea.classList.remove('current', 'future');
+        } else {
+            textArea.classList.add('future');
+            textArea.classList.remove('current', 'past');
+        }
+    });
+}
 
 
 
-// When I refresh the page, THEN the saved events persist.
+checkHour();
+setInterval(checkHour, 60 * 1000);
+
